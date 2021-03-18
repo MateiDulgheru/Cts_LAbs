d6d33a30448e368f0600c32f48f4756f326ad8f8
MateiDulgheru <dulgherumatei@gmail.com>
2021-03-18 09:25:57 +0200

    Seminar 4 final

---
+++ Seminar 4/src/ro/ase/cts/seminar4/classes/Main4.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.seminar4.classes;
+
+public class Main4 {
+	
+	public static void main(String[] args)
+	{
+		ShoppingCart myCart=ShoppingCart.getInstance();
+		myCart=ShoppingCart.getInstance();
+	}
+
+}
