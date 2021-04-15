a0ea8c0abf0f654cff2b730111e8d11ca5321b3e
MateiDulgheru <dulgherumatei@gmail.com>
2021-04-15 08:22:28 +0300

    Seminar8 Pauza

---
+++ Seminar8/src/ro/ase/cts/seminar8/main/Main8.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.seminar8.main;
+
+import ro.ase.cts.seminar8.facade.OrderFacade;
+
+public class Main8 {
+
+	public static void main(String[] args) {
+		
+		OrderFacade facade = new OrderFacade();
+		facade.order();
+		
+		
+		
+		
+	}
+
+}
